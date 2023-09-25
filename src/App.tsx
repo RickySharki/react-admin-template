0ee0ba595acedaeab4d7a5296feb91fd93802162
coldJayra <846803233@qq.com>
2023-09-25 17:41:23 +0800

    feat: 增加echarts依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "antd";
 import "./App.css";
+// import * as echarts from 'echarts';
 
 function App() {
   const [count, setCount] = useState(0);
