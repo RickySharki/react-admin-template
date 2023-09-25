43d76c8900a13967ec1d8e5bd4353dc93e2496c2
fanfuhao <1372896443@qq.com>
2023-09-25 10:55:23 +0800

    feat: 集成unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+// unocss
+import UnoCSS from "unocss/vite";
+import presetAttributify from "@unocss/preset-attributify";
+import presetIcons from "@unocss/preset-icons";
+import presetUno from "@unocss/preset-uno";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log(
+    "🚀 ~ file: vite.config.ts:13 ~ defineConfig ~ env:",
+    env,
+    command
+  );
+  return {
+    plugins: [
+      react(),
+      UnoCSS({
+        shortcuts: [
+          {
+            logo: "i-logos-react w-6em h-6em transform transition-800 hover:rotate-180",
+          },
+        ],
+        presets: [
+          presetUno(),
+          presetAttributify(),
+          presetIcons({
+            extraProperties: {
+              display: "inline-block",
+              "vertical-align": "middle",
+            },
+          }),
+        ],
+      }),
+    ],
+  };
+});
