3a0f4d3067080726a7c45f78fcaa0721415e843e
Ricky <fufufan35@gmail.com>
2023-09-28 01:02:09 +0800

    fix: 打包报错

--- src/layout/Header/components/User.tsx
+++ src/layout/Header/components/User.tsx
@@ -1,7 +1,5 @@
-import { DownOutlined, SmileOutlined } from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Dropdown, Avatar } from "antd";
-
 const UserDropDownMenu = () => {
-  return <Dropdown menu={{  }}></Dropdown>;
+  return ;
 };
+
+export default UserDropDownMenu
