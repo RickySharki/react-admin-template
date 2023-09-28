50709f93c888793dda58bb9b0a4a355cdef3d05c
Ricky Fan <fufufan35@gmail.com>
2023-09-29 01:53:53 +0800

    fix: ts报错

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // unocss注入
 import 'virtual:uno.css'
-import App from './App.tsx'
+import App from './App'
 // 样式引入
 import '@/styles/reset.scss'
 
