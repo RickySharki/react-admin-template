fd2e6cf1b7293171022dd3a9beedcf50ca6cb581
Ricky Fan <fufufan35@gmail.com>
2023-11-09 19:34:09 +0800

    feat: Update imports and language support

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import { HashRouter } from "react-router-dom";
 import Router from "@/routers/index";
 import { DevTools } from "jotai-devtools";
-import { Provider, atom } from "jotai";
-
+import { Provider, useAtom } from "jotai";
+import enUS from "antd/locale/en_US";
+import zhCN from "antd/locale/zh_CN";
 import globalStore from "@/store/global";
 import "./App.scss";
 import "./lang/i18n/config";
-// import * as echarts from 'echarts';
+import { languageAtom } from "@/store/global";
+import { Locale } from "antd/es/locale";
+
 const App = () => {
+  const [locale, setLocale] = useState<Locale>(zhCN);
+  // const [language] = useAtom(languageAtom);
+  // const isEn = language === "en";
+  // 订阅languageAtom的变化
+  globalStore.sub(languageAtom, () => {
+    console.log("countAtom value is changed to", globalStore.get(languageAtom));
+    debugger;
+    setLocale(globalStore.get(languageAtom) === "en" ? enUS : zhCN);
+  });
+
   return (
     <>
       <Provider store={globalStore}>
         <DevTools></DevTools>
         <HashRouter>
-          <ConfigProvider>
+          <ConfigProvider locale={locale}>
             <Router />
           </ConfigProvider>
         </HashRouter>
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,5 +1,5 @@
-import { atom, useAtom } from "jotai";
-import { UserInfo, MaybeIsNull, User } from "@/model/user";
+import { atom } from "jotai";
+import { User } from "@/model/user";
 
 export const userAtom = atom<User>({
   token: null,
